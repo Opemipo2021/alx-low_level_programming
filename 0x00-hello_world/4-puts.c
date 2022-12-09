1651b28df89df19bbc7f9bbe86f3e56655b25bc0
Opemipo <omoimole2020@gmail.com>
2022-12-09 03:28:17 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-
-
+#include <stdio.h>
 /**
- *
- * main - Entry Point
- *
- * return: Always 0 (Success)
- */
-
+ *  * main - prints string
+ *   * Return: Always 0 (Sucess)
+ *    */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
